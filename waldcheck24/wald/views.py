59dea1386dd97b864365ddea2e4f16c5999fb4f9
wnhrt <mail@weinhart.net>
2021-10-15 17:56:05 +0200

    Added wald overview and details

--- waldcheck24/wald/views.py
+++ waldcheck24/wald/views.py
@@ -3,7 +3,16 @@ from flask import Blueprint, render_template
 bp_wald = Blueprint('wald', __name__, url_prefix='/wald')
 
 from waldcheck24 import db
+from waldcheck24.models import *
 
 @bp_wald.route('/overview')
 def overview():
+	waelder = Wald.query.all()
 	return render_template('wald/overview.html', waelder=waelder)
+
+@bp_wald.route('/<int:wald_id>')
+def details(wald_id):
+	wald = None
+	# TODO
+	# wald = db.get_by_id(wald_id)
+	return render_template('wald/details.html', wald=wald)
