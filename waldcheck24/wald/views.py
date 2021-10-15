4e1813f6511b8f5585a9240ca33420f709acd0fe
wnhrt <mail@weinhart.net>
2021-10-15 17:32:56 +0200

    Added wald

---
+++ waldcheck24/wald/views.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template
+
+bp_wald = Blueprint('wald', __name__, url_prefix='/wald')
+
+from waldcheck24 import db
+
+@bp_wald.route('/overview')
+def overview():
+	return render_template('wald/overview.html', waelder=waelder)
