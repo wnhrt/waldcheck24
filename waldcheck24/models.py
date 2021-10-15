2851e5ce7f622fa767d9c9b87dcf7399f48dde69
wnhrt <mail@weinhart.net>
2021-10-15 18:53:27 +0200

    Fixed typos

--- waldcheck24/models.py
+++ waldcheck24/models.py
@@ -1,17 +1,17 @@
-from . import db
+from waldcheck24.app import db
 
 
-class Wald(db.model):
+class Wald(db.Model):
 	wald_id = db.Column(db.Integer, primary_key=True)
 	flaeche = db.Column(db.Float, nullable=False)
-	forstung = db.Column(db.Booelan, nullable=False)
+	forstung = db.Column(db.Boolean, nullable=False)
 	lokation = db.Column(db.String(100), nullable=False)
 	name = db.Column(db.String(100), nullable=False)
 	baumbestand = db.relationship('Baumbestand')
 	ertragsklasse = db.relationship('Ertragsklasse')
 	bewertung = db.relationship('Bewertung')
 
-class Baumbestand(db.model):
+class Baumbestand(db.Model):
 	baumbestand_id = db.Column(db.Integer, primary_key=True)
 	wald_id = db.Column(db.Integer, db.ForeignKey('wald.wald_id'), nullable=False)
 	baumart_id = db.Column(db.Integer, db.ForeignKey('baumart.baumart_id'), nullable=False)
@@ -21,18 +21,18 @@ class Baumbestand(db.model):
 	baumart = db.relationship('Baumart')
 
 
-class Baumart(db.model):
+class Baumart(db.Model):
 	baumart_id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(100), nullable=False)
 
-class Ertragsklasse(db.model):
+class Ertragsklasse(db.Model):
 	ertragsklasse_id = db.Column(db.Integer, primary_key=True)
 	wald_id = db.Column(db.Integer, db.ForeignKey('wald.wald_id'), nullable=False)
 	alter = db.Column(db.Integer, nullable=False)
 	preis = db.Column(db.Float, nullable=False)
 
 
-class Bewertung(db.model):
+class Bewertung(db.Model):
 	bewertung_id = db.Column(db.Integer, primary_key=True)
 	wald_id = db.Column(db.Integer, db.ForeignKey('wald.wald_id'), nullable=False)
 	baumbestand_id = db.Column(db.Integer, db.ForeignKey('baumbestand.baumbestand_id'), nullable=False)
