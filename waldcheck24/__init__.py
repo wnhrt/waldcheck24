f95cd36817b7a9811c48de46ea5102b5baeff830
wnhrt <mail@weinhart.net>
2021-10-15 15:51:00 +0200

    Fixed imports

--- waldcheck24/__init__.py
+++ waldcheck24/__init__.py
@@ -1,1 +1,1 @@
-from app import create_app
+from waldcheck24.app import create_app
--- waldcheck24/app.py
+++ waldcheck24/app.py
@@ -3,7 +3,7 @@ from flask import Flask
 def create_app():
 	app = Flask('waldcheck24')
 
-	from frontend import bp_frontend
+	from .frontend import bp_frontend
 	app.register_blueprint(bp_frontend)
 
 	return app
